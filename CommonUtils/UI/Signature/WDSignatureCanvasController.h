384a41aed8913ff1b575e5dc71029febc6439325
lihuaguang <lihg@shinemo.com>
2019-07-16 10:51:59 +0800

    WDSignatureCanvas
    Signed-off-by: lihuaguang <lihg@shinemo.com>

---
+++ CommonUtils/UI/Signature/WDSignatureCanvasController.h
@@ -0,0 +1,17 @@
+//
+//  WDSignatureCanvasController.h
+//  CommonUtils
+//
+//  Created by lihuaguang on 2019/7/16.
+//  Copyright © 2019 lihuaguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WDSignatureCanvasController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CommonUtils/UI/Signature/WDSignatureCanvasController.m
@@ -0,0 +1,55 @@
+//
+//  WDSignatureCanvasController.m
+//  CommonUtils
+//
+//  Created by lihuaguang on 2019/5/20.
+//  Copyright © 2019 lihuaguang. All rights reserved.
+//
+
+#import "WDSignatureCanvasController.h"
+#import "WDSignatureCanvas.h"
+#import "WDSignatureCanvas22.h"
+#import "WDSignatureCanvas33.h"
+#import "WDSignatureCanvas44.h"
+
+@interface WDSignatureCanvasController ()
+
+@property (nonatomic, strong) WDSignatureCanvas *canvasView;
+
+@end
+
+@implementation WDSignatureCanvasController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    CGRect frame = (CGRect){20, 100, self.view.bounds.size.width - 40, 500};
+    
+    self.canvasView = [[WDSignatureCanvas alloc] initWithFrame:frame];
+    self.canvasView.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:self.canvasView];
+    
+    UIButton *eraser = [UIButton buttonWithType:UIButtonTypeSystem];
+    [eraser setTitle:@"eraser" forState:UIControlStateNormal];
+    [eraser addTarget:self action:@selector(eraser) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:eraser];
+    
+    UIButton *back = [UIButton buttonWithType:UIButtonTypeSystem];
+    [back setTitle:@"back" forState:UIControlStateNormal];
+    [back addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:back];
+    
+    eraser.frame = CGRectMake(30, 620, 50, 30);
+    back.frame = CGRectMake(100, 620, 50, 30);
+}
+
+- (void)back {
+    [self.canvasView undo];
+}
+
+- (void)eraser {
+    [self.canvasView eraser];
+}
+
+@end
